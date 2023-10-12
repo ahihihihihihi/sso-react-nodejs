109c0e9349a32a669c9dadc29c22a029c78d39fa
ahihi <bdsthocu20@gmail.com>
2023-10-12 17:13:36 +0700

    #4

---
+++ src/config/connectDB.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require('sequelize');
+
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize('sso', 'root', '123456', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: '3309'
+});
+
+const connection = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+export default connection;
+
---
+++ src/controller/loginController.js
@@ -0,0 +1,9 @@
+
+
+const getLoginPage = (req, res) => {
+    return res.render("login.ejs")
+}
+
+module.exports = {
+    getLoginPage
+}
